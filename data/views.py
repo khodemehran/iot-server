74e8d60ae655c8b79ec76aa0099bd08dccae3d54
mehran bidkhori <61586215+khodemehran@users.noreply.github.com>
2020-08-10 17:59:51 +0430

    Add files via upload

--- data/views.py
+++ data/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Data
+from django.utils import timezone
+from django.db.models import Avg
 
 def home(request):
+    all_data = Data.objects.order_by('-date')
+    Temp_Avg = Data.objects.aggregate(Avg('temp'))
+    Temp_Avg = Temp_Avg["temp__avg"] 
+    Humidity_Avg = Data.objects.aggregate(Avg('humidity'))
+    Humidity_Avg = Humidity_Avg["humidity__avg"]
+    last_pk = all_data.count()
 
-    return HttpResponse('home now')
+    context = {
+        'all_data':all_data,
+        'Temp_Avg':Temp_Avg,
+        'Humidity_Avg':Humidity_Avg,
+        'last_pk':last_pk,
+        }
+
+    return render(request, 'data/home.html',context)
+
+
+
+def add_data(request):
+    if request.method == 'POST':
+        if request.POST['humidity'] and request.POST['temp']:
+            data = Data()
+            data.humidity = request.POST['humidity']
+            data.temp = request.POST['temp']
+            data.tozih = request.POST['tozih']
+            data.date = timezone.datetime.now()
+            data.save()
+            return render(request,'data/home.html')
+    else:
+        return render(request, 'data/add_data.html')
+
+        
+        
